e48a7593d8da08483b9468954177caaeff61ebfd
illia108 <illia.kuzma@gmail.com>
2017-03-07 09:55:53 +0200

    Add mobile menu and smooth scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
 $(function(){
+  $( window ).resize(function() {
+    toggleMenu();
+  });
+  
   $('#hamburger-menu').click(function(){
     $('nav').toggle();
-  })
+  });
+  
+  $('.menu-item').click(function(){
+    if ($( window ).width() < 768) { $('nav').toggle(); }
+  });
 });
+
+function toggleMenu(){
+  $( window ).width() > 768 ? $('nav').show() : $('nav').hide();
+}
