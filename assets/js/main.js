b9e5d8b9f2a2b11a3c0ed3d0d38ca432f18fcbed
illia108 <illia.kuzma@gmail.com>
2017-03-07 11:02:31 +0200

    Add animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,8 +10,23 @@ $(function(){
   $('.menu-item').click(function(){
     if ($( window ).width() < 768) { $('nav').toggle(); }
   });
+  
+  $(window).scroll(function() {
+    animateOnScrollTo($('.levels'), $('.level-circle'), 'rubberBand');
+    animateOnScrollTo($('.portfolio'), $('.work-item'), 'zoomIn');
+  });
 });
 
 function toggleMenu(){
   $( window ).width() > 768 ? $('nav').show() : $('nav').hide();
+}
+  
+function animateOnScrollTo(scrollTo, animateElement, animation) {
+  if ($( window ).width() < 768) { return; }
+  
+  var hT = scrollTo.offset().top,
+      hH = scrollTo.outerHeight(),
+      wH = $(window).height(),
+      wS = $(this).scrollTop();
+  if (wS > (hT+hH-wH)) { animateElement.addClass('animated ' + animation); }
 }
